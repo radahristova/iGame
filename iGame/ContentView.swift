ef3eedf4031371d53baed4b738e0def95f5840cd
Rada Hristova <rada.hristova@icloud.com>
2023-07-21 14:09:30 +0300

    Initial Commit

---
+++ iGame/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iGame
+//
+//  Created by Rada Hristova on 21.07.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iGame/iGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  iGameApp.swift
+//  iGame
+//
+//  Created by Rada Hristova on 21.07.23.
+//
+
+import SwiftUI
+
+@main
+struct iGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
